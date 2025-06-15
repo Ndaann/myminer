ed9a68f94c43774f067728c21223a0154bda69c9
Ndaann <najihulwildan@gmail.com>
2025-06-15 13:57:30 +0700

    Add files via upload

---
+++ start_mbc - rplant 6 thread - proporsional mode.bat
@@ -0,0 +1,5 @@
+@echo off	
+cd %~dp0	
+	
+cls	
+SRBMiner-MULTI.exe --algorithm yespower2b --pool stratum+tcps://stratum-asia.rplant.xyz:17022 --wallet BooU4WfH4pNtpVJqMkVKzyAHdy7suJf163 --password 2345 --cpu-threads 8 --disable-gpu 
